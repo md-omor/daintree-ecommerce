09c17796660d51c6107e17b4e19ff2864a4dcd90
md-omor <developeromor@gmail.com>
2022-12-22 00:30:24 +0600

    add category json

---
+++ src/modules/Home/config/constants.js
@@ -0,0 +1,18 @@
+export const category = [
+  {
+    categoryImage: "/images/category1.svg",
+    title: "Headphones",
+  },
+  {
+    categoryImage: "/images/category2.svg",
+    title: "VR Headsets",
+  },
+  {
+    categoryImage: "/images/category3.svg",
+    title: "Smartphones",
+  },
+  {
+    categoryImage: "/images/category4.svg",
+    title: "Laptops",
+  },
+];
