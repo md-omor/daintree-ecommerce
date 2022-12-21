8bb2d8c37affdb1042236b16fdfadb46f2342594
md-omor <developeromor@gmail.com>
2022-12-22 00:29:35 +0600

    home category added

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./sections/Banner";
+import Category from "./sections/Category";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Category />
     </div>
   );
 };
