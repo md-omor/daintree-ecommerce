c40fd72b15e1560b058734726cb7d8a3617da69b
md-omor <developeromor@gmail.com>
2022-12-23 02:40:57 +0600

    added brands

---
+++ src/modules/Home/sections/Brands.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { brands } from "../config/constants";
+
+const Brands = () => {
+  return (
+    <div className="md:container mx-auto pl-9 pr-8 w-full overflow-hidden mt-[100px]">
+      <h1 className="md:text-[56px] text-4xl text-center md:leading-[67px] text-[#1E1E1E] mb-6 font-black">
+        Shop by Brands
+      </h1>
+      <p className="text-[#53616E] text-2xl text-center font-bold">
+        Browse through our product catalogue and get the best value for your
+        money
+      </p>
+      <div className="flex justify-between flex-wrap mt-[100px] items-center">
+        {brands?.map(({ brandsImage }, index) => (
+          <div className="bg-[#1E1E1E] w-[300px] h-[150px] rounded-[10px] flex items-center justify-center mb-[50px]">
+            <img src={brandsImage} alt="brandsImage" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Brands;
