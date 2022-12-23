6101f31a8f3ded77a91a740b8ff7fabe8e1cc4df
md-omor <developeromor@gmail.com>
2022-12-24 02:42:05 +0600

    add catelogue on the home page

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Banner from "./sections/Banner";
+import Brands from "./sections/Brands";
 import Category from "./sections/Category";
+import Catelog from "./sections/Catelogue";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Category />
+      <Catelog />
+      <Brands />
     </div>
   );
 };
