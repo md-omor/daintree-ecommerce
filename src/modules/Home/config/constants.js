801796e1b5276efd7af2a5d1b9c0ecfb102a98ed
md-omor <developeromor@gmail.com>
2022-12-24 02:43:52 +0600

    added banner image and constants of other things

--- src/modules/Home/config/constants.js
+++ src/modules/Home/config/constants.js
@@ -16,3 +16,89 @@ export const category = [
     title: "Laptops",
   },
 ];
+
+export const catelogue = [
+  {
+    categoryImage: "/images/headphone1.svg",
+    title: "JBL Live 660NC",
+    price: "$579.99",
+    mainPrice: "$600.00",
+    rating: 4.9,
+  },
+  {
+    categoryImage: "/images/headphone2.svg",
+    title: "Sony WH-1000XM4",
+    price: "$239.99",
+    mainPrice: "$250.00",
+    rating: 4.9,
+  },
+  {
+    categoryImage: "/images/headphone1.svg",
+    title: "JBL Live 660NC",
+    price: "$579.99",
+    mainPrice: "$600.00",
+    rating: 4.9,
+  },
+  {
+    categoryImage: "/images/headphone2.svg",
+    title: "Airpods Max",
+    price: "$579.99",
+    mainPrice: "$600.00",
+    rating: 4.9,
+  },
+  {
+    categoryImage: "/images/headphone1.svg",
+    title: "JBL Live 660NC",
+    price: "$579.99",
+    mainPrice: "$600.00",
+    rating: 4.9,
+  },
+  {
+    categoryImage: "/images/headphone2.svg",
+    title: "Airpods Max",
+    price: "$579.99",
+    mainPrice: "$600.00",
+    rating: 4.9,
+  },
+  {
+    categoryImage: "/images/headphone1.svg",
+    title: "JBL Live 660NC",
+    price: "$579.99",
+    mainPrice: "$600.00",
+    rating: 4.9,
+  },
+  {
+    categoryImage: "/images/headphone2.svg",
+    title: "Airpods Max",
+    price: "$579.99",
+    mainPrice: "$600.00",
+    rating: 4.9,
+  },
+];
+
+export const brands = [
+  {
+    brandsImage: "/images/apple.svg",
+  },
+  {
+    brandsImage: "/images/dell.svg",
+  },
+  {
+    brandsImage: "/images/samsung.svg",
+  },
+  {
+    brandsImage: "/images/sony.svg",
+  },
+  {
+    brandsImage: "/images/bose.svg",
+  },
+  {
+    brandsImage: "/images/hp.svg",
+  },
+  {
+    brandsImage: "/images/asus.svg",
+  },
+  {
+    brandsImage: "/images/acer.svg",
+  },
+];
