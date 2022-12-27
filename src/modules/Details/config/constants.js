1666e4067c9f732d10c997ac422d586cb4e7a81c
md-omor <developeromor@gmail.com>
2022-12-27 18:56:15 +0600

    add product images and create product json

--- src/modules/Details/config/constants.js
+++ src/modules/Details/config/constants.js
@@ -1,30 +1,13 @@
-export const category = [
+export const productDetails = [
   {
     categoryImage: "/images/category1.svg",
-    title: "Headphones",
-  },
-  {
-    categoryImage: "/images/category1.svg",
-    title: "Headphones",
-  },
-  {
-    categoryImage: "/images/category1.svg",
-    title: "Headphones",
-  },
-  {
-    categoryImage: "/images/category1.svg",
-    title: "Headphones",
-  },
-  {
-    categoryImage: "/images/category1.svg",
-    title: "Headphones",
-  },
-  {
-    categoryImage: "/images/category1.svg",
-    title: "Headphones",
-  },
-  {
-    categoryImage: "/images/category1.svg",
-    title: "Headphones",
+    title: "ROG Zephyrus G14",
+    ratings: "4.6/5.0",
+    reviews: "737 Reviews",
+    offerPrice: "$2999.99",
+    mainPrice: "$3200.00",
+    description:
+      "The brand new 2022 Zephyrus G14 is powered by AMD and Windows 11 out of the box. Featuring new Ryzen™ 9 processors and Radeon™ RX 6800S mobile graphics, this all-AMD powerhouse is ready for any gaming or multitasking you can throw at it. The G14 also leverages AMD technologies like Smartshift and Smart Access Memory for an extra edge. AMD Smartshift allows the CPU and GPU to dynamically allocate power when and where it is needed the most, and AMD Smart Access Memory gives the CPU full access to the GPU’s full VRAM, improving performance in games.",
+    cart: "1",
   },
 ];
