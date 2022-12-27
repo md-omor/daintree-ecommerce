d7e1c23b9bc92df2eaed7be0fb07d1683ece22ea
md-omor <developeromor@gmail.com>
2022-12-27 18:55:04 +0600

    create product details

--- src/modules/Details/sections/ProductDetails.jsx
+++ src/modules/Details/sections/ProductDetails.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { productDetails } from "../config/constants";
+import FullDetails from "../partials/FullDetails";
 import ProductSlider from "../partials/ProductSlider";
 
 const ProductDetails = () => {
   return (
-    <div className="md:container mx-auto flex md:flex-nowrap flex-wrap items-start justify-between pl-9 pr-8 w-full mt-[80px] -z-50">
+    <div className="md:container mx-auto flex md:flex-nowrap flex-wrap items-start justify-start pl-9 pr-8 w-full mt-[80px] -z-50">
       <ProductSlider />
+      {productDetails?.map((data, index) => (
+        <FullDetails {...data} key={index} />
+      ))}
     </div>
   );
 };
