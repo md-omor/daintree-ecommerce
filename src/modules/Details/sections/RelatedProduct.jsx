6b4f547f79c774568dc423bcd866ad910e79e418
md-omor <developeromor@gmail.com>
2022-12-28 18:57:26 +0600

    change related product and show 4 items

--- src/modules/Details/sections/RelatedProduct.jsx
+++ src/modules/Details/sections/RelatedProduct.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { catelogue } from "../../../shared/config/constants";
 import CatelogueCard from "../../Home/partials/CatelogueCard";
 
 const RelatedProduct = () => {
@@ -7,7 +8,11 @@ const RelatedProduct = () => {
       <h1 className="font-black text-[#1E1E1E] text-[56px] leading-[57px] mb-[70px]">
         You might also like
       </h1>
-      <CatelogueCard />
+      <div className="flex flex-wrap justify-between items-center mt-[100px]">
+        {catelogue?.slice(0, 4)?.map((data, index) => (
+          <CatelogueCard {...data} key={index} />
+        ))}
+      </div>
     </div>
   );
 };
