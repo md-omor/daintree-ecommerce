b68a19fdcbe6deeff4b35b51157eaf94e52e338a
md-omor <developeromor@gmail.com>
2022-12-30 17:06:52 +0600

    fix brands responsive issue

--- src/modules/Home/sections/Brands.jsx
+++ src/modules/Home/sections/Brands.jsx
@@ -4,10 +4,10 @@ import { brands } from "../config/constants";
 const Brands = () => {
   return (
     <div className="md:container mx-auto pl-9 pr-8 w-full overflow-hidden mt-[100px]">
-      <h1 className="md:text-[56px] text-4xl text-center md:leading-[67px] text-[#1E1E1E] mb-6 font-black">
+      <h1 className="md:text-[56px] text-4xl text-left md:text-center md:leading-[67px] text-[#1E1E1E] mb-6 font-black">
         Shop by Brands
       </h1>
-      <p className="text-[#53616E] text-2xl text-center font-bold">
+      <p className="text-[#53616E] text-base md:text-2xl text-left md:text-center font-bold">
         Browse through our product catalogue and get the best value for your
         money
       </p>
