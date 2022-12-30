15e0efd808e1c2c1148df55bba6016fdada054e9
md-omor <developeromor@gmail.com>
2022-12-30 17:07:10 +0600

    fix category responsive issue

--- src/modules/Home/sections/Category.jsx
+++ src/modules/Home/sections/Category.jsx
@@ -4,15 +4,15 @@ import { category } from "../config/constants";
 const Category = () => {
   return (
     <div className="md:container mx-auto pl-9 pr-8 w-full overflow-hidden mt-[100px]">
-      <h1 className="md:text-[56px] text-4xl text-center md:leading-[67px] text-[#1E1E1E] mb-6 font-black">
+      <h1 className="md:text-[56px] text-4xl text-left md:text-center md:leading-[67px] text-[#1E1E1E] mb-6 font-black">
         Shop by Category
       </h1>
-      <p className="text-[#53616E] text-2xl text-center font-bold">
+      <p className="text-[#53616E] text-base md:text-2xl text-left md:text-center  font-bold">
         Browse through our product catalogue and get the best value for your
         money
       </p>
 
-      <div className="flex md:justify-between justify-center items-center mt-[100px] flex-wrap">
+      <div className="justify-between items-center mt-[50px] lg:gap-0 gap-[15px]  overflow-y-scroll lg:mr-0  sm:mr-[calc(768px-100vw)] lg:overflow-hidden overflow-hidden flex space-x-7 overflow-x-scroll scrollbar-hide">
         {category?.map(({ categoryImage, title }, index) => (
           <div className="relative mb-5" key={index}>
             <h1 className="text-xl text-white absolute top-5 w-full text-center font-black">
