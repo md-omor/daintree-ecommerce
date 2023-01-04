0026f3f7c45d019d4d4240c79d2617f19e8fea21
md-omor <developeromor@gmail.com>
2023-01-05 03:07:49 +0600

    create details page and add social icons

---
+++ src/modules/Details/Details.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProductDetails from "./sections/ProductDetails";
+import RelatedProduct from "./sections/RelatedProduct";
+
+const Details = () => {
+  return (
+    <div>
+      <ProductDetails />
+      <RelatedProduct />
+    </div>
+  );
+};
+
+export default Details;
